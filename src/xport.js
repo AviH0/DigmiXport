71080d02f99b07d1e86dacde60dc5238b992c4f4
Avinoam <avinoam@hershler.com>
2019-09-12 00:22:15 +0300

    Working with day parsing

--- src/xport.js
+++ src/xport.js
@@ -17,7 +17,8 @@ var dateDict = {
         "יום ג'": "20191029",
         "יום ד'": "20191030",
         "יום ה'": "20191031",
-        "יום ו'": "20191101"
+        "יום ו'": "20191101",
+        "יום ש'": ""
     },
     "סמסטר ב": {
         "יום א'": "20200315",
@@ -25,29 +26,29 @@ var dateDict = {
         "יום ג'": "20200317",
         "יום ד'": "20200318",
         "יום ה'": "20200319",
-        "יום ו'": "20200320"
+        "יום ו'": "20200320",
+        "יום ש'": ""
     }
 };
 var semesterInformation = {
     "סמסטר א": {
         "start": "",
-        "end" : "20200128"
+        "end": "20200128"
     },
     "סמסטר ב": {
         "start": "",
-        "end" : "20200730"
+        "end": "20200730"
     },
     "שנתי": {
         "start": "",
-        "end" : "20200128"
+        "end": "20200128"
     }
 };
-var endOfSemester = {"סמסטר ב": "20200730", "סמסטר א": "20200128","שנתי": "20200128"};
+var endOfSemester = {"סמסטר ב": "20200730", "סמסטר א": "20200128", "שנתי": "20200128"};
 
 xhr = new XMLHttpRequest();
 xhr.open('GET', "https://academic-secretary.huji.ac.il/%D7%9C%D7%95%D7%97-%D7%94%D7%A9%D7%A0%D7%94-%D7%94%D7%90%D7%A7%D7%93%D7%9E%D7%99%D7%AA");
 xhr.addEventListener('loadend', tableLoaded);
-xhr.addEventListener("error", tableLoaded);
 xhr.send();
 
 function tableLoaded(e) {
@@ -57,56 +58,64 @@ function tableLoaded(e) {
     table = hujiTable.getElementsByTagName('table')[0];
     tableRows = table.getElementsByTagName('tr');
 
-    for(row=0;row< tableRows.length;row++){
+    for (row = 0; row < tableRows.length; row++) {
 
         eventName = tableRows[row].cells[0].textContent;
-        if(eventName.includes("פתיחת שנת הלימודים")){
+        if (eventName.includes("פתיחת שנת הלימודים")) {
             semesterAbeginsAt = tableRows[row].cells[2].textContent;
             semesterAbeginsAtDates = semesterAbeginsAt.split('.');
 
-            semesterAbeginsAtDates[0] =makeTwoDigits(semesterAbeginsAtDates[0]);
-            semesterAbeginsAtDates[1] =makeTwoDigits(semesterAbeginsAtDates[1]);
-            semesterAbeginsAtDates[2] =makeTwoDigits(semesterAbeginsAtDates[2]);
+            semesterAbeginsAtDates[0] = makeTwoDigits(semesterAbeginsAtDates[0]);
+            semesterAbeginsAtDates[1] = makeTwoDigits(semesterAbeginsAtDates[1]);
+            semesterAbeginsAtDates[2] = makeTwoDigits(semesterAbeginsAtDates[2]);
 
 
-            semesterAbeginsAt = year_prefix + semesterAbeginsAtDates[2]+ semesterAbeginsAtDates[1] + semesterAbeginsAtDates[0];
-            semesterAbeginsAt=semesterAbeginsAt.trim();
+            semesterAbeginsAt = year_prefix + semesterAbeginsAtDates[2] + semesterAbeginsAtDates[1] + semesterAbeginsAtDates[0];
+            semesterAbeginsAt = semesterAbeginsAt.trim();
+            findDays("סמסטר א", year_prefix + semesterAbeginsAtDates[2], semesterAbeginsAtDates[1], semesterAbeginsAtDates[0]);
+            dateDict["שנתי"]=dateDict["סמסטר א"];
+
         }
-        if(eventName.includes("סיום סמסטר א'")){
+        if (eventName.includes("סיום סמסטר א'")) {
             semesterAendsAt = tableRows[row].cells[2].textContent;
             semesterAendsAtDates = semesterAendsAt.split('.');
 
-            semesterAendsAtDates[0] =makeTwoDigits(semesterAendsAtDates[0]);
-            semesterAendsAtDates[1] =makeTwoDigits(semesterAendsAtDates[1]);
-            semesterAendsAtDates[2] =makeTwoDigits(semesterAendsAtDates[2]);
+            semesterAendsAtDates[0] = makeTwoDigits(semesterAendsAtDates[0]);
+            semesterAendsAtDates[1] = makeTwoDigits(semesterAendsAtDates[1]);
+            semesterAendsAtDates[2] = makeTwoDigits(semesterAendsAtDates[2]);
 
 
             semesterAendsAt = year_prefix + semesterAendsAtDates[2] + semesterAendsAtDates[1] + semesterAendsAtDates[0];
-            semesterAendsAt=semesterAendsAt.trim();
+            semesterAendsAt = semesterAendsAt.trim();
         }
-        if(eventName.includes("פתיחת סמסטר ב'")){
+        if (eventName.includes("פתיחת סמסטר ב'")) {
             semesterBbeginsAt = tableRows[row].cells[2].textContent;
             semesterBbeginsAtDates = semesterBbeginsAt.split('.');
 
-            semesterBbeginsAtDates[0] =makeTwoDigits(semesterBbeginsAtDates[0]);
-            semesterBbeginsAtDates[1] =makeTwoDigits(semesterBbeginsAtDates[1]);
-            semesterBbeginsAtDates[2] =makeTwoDigits(semesterBbeginsAtDates[2]);
+            semesterBbeginsAtDates[0] = makeTwoDigits(semesterBbeginsAtDates[0]);
+            semesterBbeginsAtDates[1] = makeTwoDigits(semesterBbeginsAtDates[1]);
+            semesterBbeginsAtDates[2] = makeTwoDigits(semesterBbeginsAtDates[2]);
 
-            semesterBbeginsAt =year_prefix + semesterBbeginsAtDates[2] + semesterBbeginsAtDates[1] +  semesterBbeginsAtDates[0];
-            semesterBbeginsAt=semesterBbeginsAt.trim();
+            semesterBbeginsAt = year_prefix + semesterBbeginsAtDates[2] + semesterBbeginsAtDates[1] + semesterBbeginsAtDates[0];
+            semesterBbeginsAt = semesterBbeginsAt.trim();
+
+            findDays("סמסטר ב", year_prefix + semesterBbeginsAtDates[2], semesterBbeginsAtDates[1], semesterBbeginsAtDates[0]);
+
+
+
         }
-        if(eventName.includes("סיום סמסטר ב'")){
+        if (eventName.includes("סיום סמסטר ב'")) {
             semesterBendsAt = tableRows[row].cells[2].textContent;
             semesterBendsAtDates = semesterBendsAt.split('.');
 
-            semesterBendsAtDates[0] =makeTwoDigits(semesterBendsAtDates[0]);
-            semesterBendsAtDates[1] =makeTwoDigits(semesterBendsAtDates[1]);
-            semesterBendsAtDates[2] =makeTwoDigits(semesterBendsAtDates[2]);
+            semesterBendsAtDates[0] = makeTwoDigits(semesterBendsAtDates[0]);
+            semesterBendsAtDates[1] = makeTwoDigits(semesterBendsAtDates[1]);
+            semesterBendsAtDates[2] = makeTwoDigits(semesterBendsAtDates[2]);
 
             semesterBendsAt = year_prefix + semesterBendsAtDates[2] + semesterBendsAtDates[1] + semesterBendsAtDates[0];
-            semesterBendsAt=semesterBendsAt.trim();
+            semesterBendsAt = semesterBendsAt.trim();
         }
-        if(tableRows[row].cells[3].textContent.includes("לא יתקיימו לימודים או בחינות")) {
+        if (tableRows[row].cells[3].textContent.includes("לא יתקיימו לימודים או בחינות")) {
             exclusionDate = tableRows[row].cells[2].textContent;
 
             // Parse which dates are to be excluded. this is fun.
@@ -116,12 +125,12 @@ function tableLoaded(e) {
                 if (dateRange[0].split('.').length == 1) {
                     endRange = dateRange[1].split('.');
                     for (i = parseInt(dateRange[0]); i <= parseInt(endRange[0]); i++) {
-                        ii =  i.toString();
+                        ii = i.toString();
 
                         ii = makeTwoDigits(ii);
                         endRange[1] = makeTwoDigits(endRange[1]);
 
-                        exclusionDate =  year_prefix + endRange[2] + endRange[1] +ii;
+                        exclusionDate = year_prefix + endRange[2] + endRange[1] + ii;
                         exclusionDates.push(exclusionDate);
                     }
                 } else {
@@ -129,32 +138,31 @@ function tableLoaded(e) {
                     endRange = dateRange[1].split('.');
                     if (parseInt(startRange[0]) < parseInt(startRange[1])) {
                         for (i = parseInt(startRange[0]); i < 32; i++) {
-                            ii =  i.toString();
+                            ii = i.toString();
 
                             ii = makeTwoDigits(ii);
                             startRange[1] = makeTwoDigits(startRange[1]);
 
-                            exclusionDate =  year_prefix + startRange[2] + startRange[1] +ii;
+                            exclusionDate = year_prefix + startRange[2] + startRange[1] + ii;
                             exclusionDates.push(exclusionDate);
                         }
                         for (i = 1; i <= parseInt(endRange[0]); i++) {
-                            ii =  i.toString();
+                            ii = i.toString();
 
                             ii = makeTwoDigits(ii);
                             endRange[1] = makeTwoDigits(endRange[1]);
 
-                            exclusionDate = year_prefix + endRange[2]+ endRange[1] + ii;
+                            exclusionDate = year_prefix + endRange[2] + endRange[1] + ii;
                             exclusionDates.push(exclusionDate);
                         }
-                    }
-                    else{
+                    } else {
                         for (i = parseInt(startRange[0]); i < parseInt(endRange[0]); i++) {
-                            ii =  i.toString();
+                            ii = i.toString();
 
                             ii = makeTwoDigits(ii);
                             startRange[1] = makeTwoDigits(startRange[1]);
 
-                            exclusionDate =  year_prefix + startRange[2] + startRange[1] +ii;
+                            exclusionDate = year_prefix + startRange[2] + startRange[1] + ii;
                             exclusionDates.push(exclusionDate);
                         }
                     }
@@ -164,7 +172,7 @@ function tableLoaded(e) {
                 exclusionDateDates[0] = makeTwoDigits(exclusionDateDates[0]);
                 exclusionDateDates[1] = makeTwoDigits(exclusionDateDates[1]);
 
-                exclusionDate =year_prefix + exclusionDateDates[2] + exclusionDateDates[1] +  exclusionDateDates[0];
+                exclusionDate = year_prefix + exclusionDateDates[2] + exclusionDateDates[1] + exclusionDateDates[0];
                 exclusionDate = exclusionDate.trim();
                 exclusionDates.push(exclusionDate);
             }
@@ -177,23 +185,49 @@ function tableLoaded(e) {
     semesterInformation = {
         "סמסטר א": {
             start: semesterAbeginsAt,
-            end : semesterAendsAt
+            end: semesterAendsAt
         },
         "סמסטר ב": {
             start: semesterBbeginsAt,
-            end : semesterBendsAt
+            end: semesterBendsAt
         },
         "שנתי": {
             start: semesterAbeginsAt,
-            end : semesterBendsAt
+            end: semesterBendsAt
         }
     };
     exportCal();
 }
 
-function makeTwoDigits(number){
+function findDays(semester, year, month, date) {
+    datevar = new Date();
+    datevar.setFullYear(parseInt(year),parseInt(month)-1,parseInt(date));
+    day = datevar.getDay();
+
+    daysDict = {
+        0: "יום א'",
+        1: "יום ב'",
+        2: "יום ג'",
+        3: "יום ד'",
+        4: "יום ה'",
+        5: "יום ו'",
+        6: "יום ש'"
+    };
+    TWENTYFOURHOURS = 86400000;
+    dateDict[semester][daysDict[day]] = year + month + date;
+    datevar = new Date(datevar.getTime()+TWENTYFOURHOURS);
+    for(i=(day+1)%7; i!=day;i=(i+1)%7){
+        datevar = new Date(datevar.getTime()+TWENTYFOURHOURS);
+        dateDict[semester][daysDict[i]] = datevar.getFullYear().toString() + makeTwoDigits((datevar.getMonth()+1).toString()) + makeTwoDigits(datevar.getDate().toString());
+    }
+
+
+
+}
+
+function makeTwoDigits(number) {
     number = number.trim();
-    if(number.length<2){
+    if (number.length < 2) {
         return '0' + number;
     }
     return number;
@@ -220,7 +254,7 @@ function exportCal() {
                 i++;
                 if (i == j) {
                     // window.open("data:text/calendar;charset=utf-8," + escape(calendar));
-                    download_file("Calendar.ics", calendar+"END:VCALENDAR");
+                    download_file("Calendar.ics", calendar + "END:VCALENDAR");
                 }
             }
         });
@@ -253,7 +287,7 @@ function extractData(courseData) {
                 lessonSemester = hours[hour].semester;
 
                 //date = translateDayToDate(hours[hour].day, hours[hour].semester);
-                date = semesterInformation[lessonSemester].start;
+                date = dateDict[lessonSemester][hours[hour].day];
 
                 timeInDay = hours[hour].hour.split('-');
 
@@ -268,11 +302,10 @@ function extractData(courseData) {
                 // In some cases, the times are in the wrong order, so we should check the order.
                 aTimeInt = parseInt(aTime);
                 bTimeInt = parseInt(bTime);
-                if(aTimeInt<bTimeInt){
+                if (aTimeInt < bTimeInt) {
                     startTime = aTime;
                     endTime = bTime;
-                }
-                else {
+                } else {
                     startTime = bTime;
                     endTime = aTime;
                 }
@@ -281,10 +314,9 @@ function extractData(courseData) {
                 start = date + "T" + startTime + '00';
                 end = date + "T" + endTime + '00';
                 title = lessonType + ": " + courseId + " -- " + courseName;
-                iCalEvent += createIcalEvent(title, start, end, place, semesterInformation[lessonSemester].end,hours[hour].day);
-            }
-            catch (e) {
-                console.error("Could not create event for course "+ courseId + ", reason:\n" + e.message);
+                iCalEvent += createIcalEvent(title, start, end, place, semesterInformation[lessonSemester].end);
+            } catch (e) {
+                console.error("Could not create event for course " + courseId + ", reason:\n" + e.message);
             }
         }
     }
@@ -313,28 +345,26 @@ var lessonTypeDict = {
 };
 
 
-
 function translateDayToDate(day, semester) {
     return dateDict[semester][day];
 
 }
 
-function createIcalEvent(title, start, end, place, endOfSemester, byDay) {
+function createIcalEvent(title, start, end, place, endOfSemester) {
     // decode html entities in "place" string
     // place = $('<div>').html(place).text();
 
-    startTime = start.substr(9, start.length-1);
+    startTime = start.substr(9, start.length - 1);
 
     var result = "BEGIN:VEVENT\n";
     result += "DTSTART:" + start + "\n";
     result += "DTEND:" + end + "\n";
-    byDay = dateDict["סמסטר א"][byDay];
-    result += "RRULE:FREQ=WEEKLY;BYDAY="+byDay+";INTERVAL=1;" + "UNTIL=" + endOfSemester + "T000000\n";
+    result += "RRULE:FREQ=WEEKLY;INTERVAL=1;UNTIL=" + endOfSemester + "T000000\n";
     result += "EXDATE:";
-    for(i=0;i<exclusionDates.length-1;i++){
-        result+= exclusionDates[i] + 'T'+ startTime + ',';
+    for (i = 0; i < exclusionDates.length - 1; i++) {
+        result += exclusionDates[i] + 'T' + startTime + ',';
     }
-    result+=exclusionDates[i]+ '\n';
+    result += exclusionDates[i] + 'T' + startTime + '\n';
     result += "SUMMARY:" + title + "\n";
     result += "LOCATION:" + place + "\n";
     result += "END:VEVENT\n";
