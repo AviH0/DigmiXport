60899b29cacf15ca450f9ced34f1a98c8ee56530
Avinoam <avinoam@hershler.com>
2021-08-25 18:07:13 +0300

    Update academic schedule url

--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,5 @@
 var parsedCalendar;
-var schediuleURL = "https://academic-secretary.huji.ac.il/%D7%9C%D7%95%D7%97-%D7%90%D7%A7%D7%93%D7%9E%D7%99-%D7%9C%D7%A9%D7%A0%D7%AA-%D7%94%D7%9C%D7%99%D7%9E%D7%95%D7%93%D7%99%D7%9D-%D7%AA%D7%A9%D7%A4%D7%90-202021"
+var schediuleURL = "https://academic-secretary.huji.ac.il/%D7%9C%D7%95%D7%97-%D7%94%D7%A9%D7%A0%D7%94-%D7%94%D7%90%D7%A7%D7%93%D7%9E%D7%99%D7%AA"
 var year = "2022"; // NEED TO CHANGE THIS FOR NEXT YEAR
 load();
 
